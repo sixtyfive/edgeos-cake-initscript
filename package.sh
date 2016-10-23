5153496a912a06a04974102367d040450b7b3f76
J. R. Schmid <jrs+git@weitnahbei.de>
2016-10-24 00:25:33 +0200

    Adding README. Removing binary .tar.gz's after packaging.

--- package.sh
+++ package.sh
@@ -4,4 +4,8 @@ wget -c http://static.lochnair.net/bufferbloat/iproute2-e100_e200.tar.gz
 wget -c http://static.lochnair.net/bufferbloat/sch_cake-e100.tar.gz
 tar xvzf iproute2-e100_e200.tar.gz
 tar xvzf sch_cake-e100.tar.gz
+sudo chown -R root:root config etc lib sbin
+sudo chmod 0755 sbin/{ip,tc}
 sudo tar cvzf cake-binaries-modules-and-scripts-ERLite3-only.tar.gz config etc lib sbin
+sudo chown -R $USER:$USER config etc lib sbin
+rm iproute2-e100_e200.tar.gz sch_cake-e100.tar.gz
