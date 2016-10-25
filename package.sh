442dd10fc376dc5c42da97c0d9cf9de3a2248885
J. R. Schmid <jrs+git@weitnahbei.de>
2016-10-25 10:34:21 +0200

    Moving script to a more standard EdgeOS place.

--- package.sh
+++ package.sh
@@ -7,5 +7,6 @@ tar xvzf sch_cake-e100.tar.gz
 sudo chown -R root:root config etc lib sbin
 sudo chmod 0755 sbin/{ip,tc}
 sudo tar cvzf cake-binaries-modules-and-scripts-ERLite3-only.tar.gz config etc lib sbin
+zip -0 cake-binaries-modules-and-scripts-ERLite3-only.zip cake-binaries-modules-and-scripts-ERLite3-only.tar.gz
 sudo chown -R $USER:$USER config etc lib sbin
 rm iproute2-e100_e200.tar.gz sch_cake-e100.tar.gz
